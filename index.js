7fbad8046078da3340045c3ce74e07f0fe47f3d3
Lokhin Wu <lokhin.wu@live.com>
2017-02-18 19:01:10 -0500

    wd

--- index.js
+++ index.js
@@ -1,46 +1,101 @@
 let express = require("express"),
     request = require("request"),
-    myIP = require('my-ip');
+    externalip = require("externalip");
 bodyparser = require("body-parser");
 
 let app = express();
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
-
-app.get("/", function(req, res) {
-    try {
-        let location = '138.51.147.31';
-        console.log(location);
-        let location_url = "http://dataservice.accuweather.com/locations/v1/cities/ipaddress?apikey=doss23s1LxQl8yiV7YgQu4uK3lU2Ak2v&q=" + location + "&language=en-us&details=false"
-        request(location_url, function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                let random_playlist = Math.floor(Math.random() * 4) + 1;
-                let location_key = JSON.parse(body).Key;
-                let location_name = JSON.parse(body).EnglishName + ", " + JSON.parse(body).Country.EnglishName;
-                let weather_url = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + location_key + "?apikey=doss23s1LxQl8yiV7YgQu4uK3lU2Ak2v&language=en-us&details=false&metric=true";
-                request(weather_url, function(error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        res.render("result", {
-                            result: JSON.parse(body),
-                            location_name: location_name,
-                            random_playlist: random_playlist
-                        });
-                    }
-                });
-            }
-        });
-    } catch (err) {
-        console.log(err);
-    }
-});
-
 app.use(bodyparser.urlencoded({
     extended: true
 }));
 
-app.post("/result", function(req, res) {
+app.get("/", function(req, res) {
+    externalip(function(err, ip) {
+        try {
+            let location = ip;
+            console.log(location);
+            let location_url = "http://dataservice.accuweather.com/locations/v1/cities/ipaddress?apikey=VrE0nYTPMGbpW5JBj358TJxrsUlSOScr&q=" + location + "&language=en-us&details=false"
+            request(location_url, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    let random_playlist = Math.floor(Math.random() * 4) + 1;
+                    let location_key = JSON.parse(body).Key;
+                    let location_name = JSON.parse(body).EnglishName + ", " + JSON.parse(body).Country.EnglishName;
+                    let weather_url = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + location_key + "?apikey=VrE0nYTPMGbpW5JBj358TJxrsUlSOScr&language=en-us&details=false&metric=true";
+                    request(weather_url, function(error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            res.render("dashboard", {
+                                result: JSON.parse(body),
+                                location_name: location_name,
+                                random_playlist: random_playlist
+                            });
+                        }
+                    });
+                }
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    });
+});
 
+app.get("/weather", function(req, res) {
+    externalip(function(err, ip) {
+        try {
+            let location = ip;
+            console.log(location);
+            let location_url = "http://dataservice.accuweather.com/locations/v1/cities/ipaddress?apikey=VrE0nYTPMGbpW5JBj358TJxrsUlSOScr&q=" + location + "&language=en-us&details=false"
+            request(location_url, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    let random_playlist = Math.floor(Math.random() * 4) + 1;
+                    let location_key = JSON.parse(body).Key;
+                    let location_name = JSON.parse(body).EnglishName + ", " + JSON.parse(body).Country.EnglishName;
+                    let weather_url = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + location_key + "?apikey=VrE0nYTPMGbpW5JBj358TJxrsUlSOScr&language=en-us&details=false&metric=true";
+                    request(weather_url, function(error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            res.render("weather", {
+                                result: JSON.parse(body),
+                                location_name: location_name,
+                                random_playlist: random_playlist
+                            });
+                        }
+                    });
+                }
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    });
+});
+
+app.get("/music", function(req, res) {
+    externalip(function(err, ip) {
+        try {
+            let location = ip;
+            console.log(location);
+            let location_url = "http://dataservice.accuweather.com/locations/v1/cities/ipaddress?apikey=VrE0nYTPMGbpW5JBj358TJxrsUlSOScr&q=" + location + "&language=en-us&details=false"
+            request(location_url, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    let random_playlist = Math.floor(Math.random() * 4) + 1;
+                    let location_key = JSON.parse(body).Key;
+                    let location_name = JSON.parse(body).EnglishName + ", " + JSON.parse(body).Country.EnglishName;
+                    let weather_url = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + location_key + "?apikey=VrE0nYTPMGbpW5JBj358TJxrsUlSOScr&language=en-us&details=false&metric=true";
+                    request(weather_url, function(error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            res.render("music", {
+                                result: JSON.parse(body),
+                                location_name: location_name,
+                                random_playlist: random_playlist
+                            });
+                        }
+                    });
+                }
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    });
 });
 
 app.listen(3000, function(err) {
